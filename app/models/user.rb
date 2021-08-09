43412df36fe969c532b3a7713334bcdad4e6816d
Charles <c.friend85@yahoo.com>
2021-08-09 14:24:06 -0600

    Fixed relationship issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ActiveRecord::Base
   validates :password, length: {maximum:72}, on: :create, presence: true
   validates :password_confirmation, on: :create, length: {maximum:72}, presence: true
   has_many :hikes, dependent: :destroy
-  has_many :hikes_joined, through: :joins, source: :hike
+  has_many :hikes_joined, through: :joins, source: :hike, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :joins, dependent: :destroy
 end
